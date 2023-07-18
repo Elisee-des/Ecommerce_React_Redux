04314b108673696a3e3191d02048745782100737
Elisee-des <esabidani@gmail.com>
2023-07-18 18:18:19 +0000

    Speacial et product part

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Marquee from 'react-fast-marquee';
 import { Link } from 'react-router-dom';
 import BlogCard from '../components/BlogCard';
+import ProductCard from '../components/ProductCard';
+import SpecialProduct from '../components/SpecialProduct';
 
 const Home = () => {
     return (
@@ -178,6 +180,97 @@ const Home = () => {
                 </div>
             </section>
 
+            <section className="featred-wrapper py-5 home-wrapper-2">
+                <div className="container-xxl">
+                    <div className="row">
+                        <div className="col-12">
+                            <h3 className="section-heading">Featured Collection</h3>
+                        </div>
+                            <ProductCard />
+                            <ProductCard />
+                            <ProductCard />
+                            <ProductCard />
+                    </div>
+                </div>
+            </section> 
+
+            <section className="famous-wrapper py-5 home-wrapper-2">
+                <div className="container-xxl">
+                    <div className="row">
+                        <div className="col-3">
+                            <div className="famous-card position-relative">
+                                <img src="images/tab1.jpg" className='img-fluid' alt="" />
+                                <div className="famous-content position-absolute">
+                                    <h5>Big Screen</h5>
+                                    <h6>Smart Watch Series 7</h6>
+                                    <p>From $399 or $16.5/mo for 24 mo.*</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-3">
+                            <div className="famous-card position-relative">
+                                <img src="images/tab3.jpg" className='img-fluid' alt="" />
+                                <div className="famous-content position-absolute">
+                                    <h5>Studio Display</h5>
+                                    <h6>600 nits of brightness</h6>
+                                    <p>27-inch 5k Retina display</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-3">
+                            <div className="famous-card position-relative">
+                                <img src="images/tab3.jpg" className='img-fluid' alt="" />
+                                <div className="famous-content position-absolute">
+                                    <h5>Smartphones</h5>
+                                    <h6>Smartphone 13 pro</h6>
+                                    <p>Now in green From $999.00 or 41.5$/mo</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-3">
+                            <div className="famous-card position-relative">
+                                <img src="images/tab1.jpg" className='img-fluid' alt="" />
+                                <div className="famous-content position-absolute">
+                                    <h5>home speaker</h5>
+                                    <h6>Room-filling sound</h6>
+                                    <p>From $650 or $34.4/mo</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section className="special-wrapper py-5 home-wrapper-2">
+                <div className="container-xxl">
+                    <div className="row">
+                        <div className="col-12">
+                            <h3 className="section-heading">Specials Products</h3>
+                        </div>
+                            <div className="row">
+                                <SpecialProduct />
+                                <SpecialProduct />
+                                <SpecialProduct />
+                                <SpecialProduct />
+                            </div>
+                    </div>
+                </div>
+            </section>
+
+            <section className="popular-wrapper py-5 home-wrapper-2">
+                <div className="container-xxl">
+                    <div className="row">
+                        <div className="col-12">
+                            <h3 className="section-heading">Our Popular Products</h3>
+                        </div>
+                            <ProductCard />
+                            <ProductCard />
+                            <ProductCard />
+                            <ProductCard />
+                    </div>
+                </div>
+            </section> 
+
             <section className="marquee-wrapper py-5">
                 <div className="container-xxl">
                     <div className="row">
@@ -218,6 +311,9 @@ const Home = () => {
             <section className="blog-wrapper py-5 home-wrapper-2">
                 <div className="container-xxl">
                     <div className="row">
+                        <div className="col-12">
+                            <h3 className="section-heading">Our Last Blogs</h3>
+                        </div>
                             <BlogCard />
                             <BlogCard />
                             <BlogCard />
