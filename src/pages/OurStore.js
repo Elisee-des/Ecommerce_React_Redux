9bf6a1f66999219bf4b4d5299e224a4f9830ef6f
Elisee-des <esabidani@gmail.com>
2023-07-18 18:48:14 +0000

    Component pour Meta du titre et breadCrumb

---
+++ src/pages/OurStore.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import BreadCrumb from '../components/BreadCrumb';
+import Meta from '../components/Meta';
+
+const OurStorie = () => {
+    return (
+        <>
+            <Meta title="Our store"/>
+            <BreadCrumb title="Our store"/>
+        </>
+    );
+};
+
+export default OurStorie;
